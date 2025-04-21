24f42ea7dbd2769f4897a675d70a48048ad003a6
Neil Goodman <neilgoodman@live.com>
2025-04-20 21:13:50 -0300

    ajustes referentes ao vercel (configuração e script da build)

--- build.sh
+++ build.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
+# Ativar ambiente virtual Python
+python -m venv venv
+source venv/bin/activate
+
 # Instalar dependências Python
 pip install -r requirements.txt
+
+# Aplicar migrações do Django
+python manage.py migrate
 
 # Instalar dependências Node e fazer build do frontend
 cd portal-apoio-vite
 npm install
-npm run build 
+npm run build
+
+# Voltar para o diretório raiz
+cd ..
+
+# Coletar arquivos estáticos do Django
+python manage.py collectstatic --noinput 
